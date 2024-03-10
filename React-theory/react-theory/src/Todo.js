c2c2e251982c9d7f815e61a1a57b458d9f079238
Reema Dhanwani <reemadhanwani55@gmail.com>
2024-03-10 18:30:00 +0530

    todo app example

--- React-theory/react-theory/src/Todo.js
+++ React-theory/react-theory/src/Todo.js
@@ -4,13 +4,37 @@ import {useState} from "react";
 function Todo(props){
   //const todoItems = props.todoList.map((todo)=><TodoItem key={todo.id} todo={todo}/>)
 
+  function deleteTodo(id){
+    //console.log(id);
+    let remainingTodos = todoItem.filter((todo)=>todo.id !== id);
+    updateTodo(remainingTodos);
+  }
+
+  function editTodo(todoId,todoData){
+    let updateTodoItems = todoItem.map((todo)=>{
+      if(todo.id == todoId) todo.data = todoData;
+      return todo;
+    });
+    updateTodo(updateTodoItems);
+  }
+
   const [todoItem,updateTodo] = useState([{data:"Todo2",id:736},{data:"Todo1",id:123}]);
   const [inputText,updateText] = useState("");
   return (
   <>
      <input type='text' value={inputText} onChange={(event)=>updateText(event.target.value)}></input>
      <button onClick={()=>updateTodo([...todoItem,{data:inputText,id:(new Date()).getTime()}])}>Add Todo</button>
-     <ul>{todoItem.map((todo) => <TodoItem todo={todo} key={todo.id}/>)}</ul>
+    {/** <ul>{todoItem.map((todo) => <TodoItem todo={todo} key={todo.id}/>)}</ul> */}
+    {/**
+     * passing information from child to parent
+     * it's bit tricky
+     * we will pass a callback
+     */} 
+     <ul>{todoItem.map((todo) => <TodoItem 
+                                    todo={todo} 
+                                    key={todo.id} 
+                                    delete={()=>deleteTodo(todo.id)}
+                                    edit={(newTodo)=>editTodo(todo.id,newTodo)}/>)}</ul>
   </>
   );
 }
--- React-theory/react-theory/src/TodoItem.js
+++ React-theory/react-theory/src/TodoItem.js
@@ -6,20 +6,40 @@ function handleClick(todo){
 }
 
 function TodoItem(props) {
-    const [isFinished,setFinished] = useState(false);
+   // const [isFinished,setFinished] = useState(false);
+   const [isEditing,setIsEditing] = useState(false);
+   const [todoData,setTodoData] = useState(props.todo.data);
     return (
         <div>
-           {/** 
-            * <li onClick={(event)=>{
-                console.log(event);
-                handleClick(props.todo.data)}}>
-                    <span class={(isFinished)?'todo-done':'todo-notDone'}>{props.todo.data}</span></li>
-           */} 
+            {/*
            <li onClick={(event)=>{
                 console.log(event);
                 handleClick(props.todo.data)}}>
-                    <span class={(isFinished)?'todo-done':'todo-notDone'}>{props.todo.data}</span></li>
-            <button onClick={()=>setFinished(!isFinished)}>{(isFinished)?'Redo':'Done'}</button>
+                    <span className={(isFinished)?'todo-done':'todo-notDone'}>{props.todo.data}</span></li>
+            
+            <button onClick={()=>{
+                setFinished(!isFinished)
+                props.delete()
+            }}>{(isFinished)?'Redo':'Done'}</button>
+        */}
+
+            <li>
+                {
+                    (isEditing)?<input type="text" value={todoData}
+                                       onChange = {(event)=>setTodoData(event.target.value)}/>:<span>{todoData}</span>
+                }
+                
+            </li>
+            <button onClick={()=>{
+                props.delete()
+            }}>Done</button>
+
+            <button onClick={()=>{
+                setIsEditing(!isEditing);
+                props.edit(todoData)
+            }}>
+                {(isEditing)?'Save':'Edit'}
+            </button>
         </div>
     )
 }
